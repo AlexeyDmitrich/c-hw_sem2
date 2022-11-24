95a248dc0f28879fcec4dea68ff79aff7b320736
/dev/null <a.d.kamenev@yandex.ru>
2022-11-24 21:10:58 +0300

    seminar2 complited

---
+++ q1/Program.cs
@@ -0,0 +1,11 @@
+﻿// вывести случ 3зн число и убрать 2ю цифру из него
+
+Random rnd = new Random();
+
+int num = rnd.Next(100,1000);
+int first = num / 100;
+int last = num % 10;
+int res = (first * 10)+last;
+
+Console.WriteLine(num);
+Console.WriteLine(res);
---
+++ q2/Program.cs
@@ -0,0 +1,14 @@
+﻿// принимаем два числа, проверяем, кратность
+
+Console.WriteLine("Введите число");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите ещё число");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 % num2 == 0) {
+    Console.WriteLine ("Числа кратны");
+} else {
+    Console.WriteLine ("Числа не кратны");
+    Console.Write("Остаток: ");
+    onsole.WriteLine (num1 % num2);
+}
